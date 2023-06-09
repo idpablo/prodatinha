2ab79852935dd06cfe2589fe10f76425ee501a95
idpablo <pbs.97@outlook.com>
2023-06-09 16:06:20 -0300

    Configurando logs

--- prodatinha.py
+++ prodatinha.py
@@ -1,6 +1,5 @@
 import subprocess
 import platform
-import logging
 import random
 import json
 import sys
@@ -25,64 +24,20 @@ bot = Bot(
     help_command=None,
 )
 
-class LoggingFormatter(logging.Formatter):
-    # Colors
-    black = "\x1b[30m"
-    red = "\x1b[31m"
-    green = "\x1b[32m"
-    yellow = "\x1b[33m"
-    blue = "\x1b[34m"
-    gray = "\x1b[38m"
-    # Styles
-    reset = "\x1b[0m"
-    bold = "\x1b[1m"
+from logger import setup_logger
 
-    COLORS = {
-        logging.DEBUG: gray + bold,
-        logging.INFO: blue + bold,
-        logging.WARNING: yellow + bold,
-        logging.ERROR: red,
-        logging.CRITICAL: red + bold,
-    }
-
-    def format(self, record):
-        log_color = self.COLORS[record.levelno]
-        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
-        format = format.replace("(black)", self.black + self.bold)
-        format = format.replace("(reset)", self.reset)
-        format = format.replace("(levelcolor)", log_color)
-        format = format.replace("(green)", self.green + self.bold)
-        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
-        return formatter.format(record)
-
-
-logger = logging.getLogger("discord_bot")
-logerror = logging.getLogger("discord_bot_error")
-logger.setLevel(logging.INFO)
-logerror.setLevel(logging.ERROR)
-
-# Console handler
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(LoggingFormatter())
-# File handler
-file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-file_handler_formatter = logging.Formatter(
-    "[{asctime}] [{levelname:<4}] {name}: {message}", "%d-%m-%Y %H:%M:%S", style="{"
-)
-file_handler.setFormatter(file_handler_formatter)
-
-# Add the handlers
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+# Setup loggers
+logger = setup_logger("discord_bot", "discord.log")
+logerror = setup_logger("discord_bot_error", "discord.log")
 
 bot.logger = logger
 bot.logerror = logerror
 bot.config = config
 
+bot.config = config
+
 # Diretório do projeto Java
 diretorio_projeto = "C:/prodata/sig/"
-
-teste   
 
 @bot.event
 async def on_ready():
