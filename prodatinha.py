a448b2a0dc1c6897848d9f409188cb861ebffb7b
idpablo <pbs.97@outlook.com>
2023-06-09 15:38:50 -0300

    Initial commit

---
+++ prodatinha.py
@@ -0,0 +1,267 @@
+import subprocess
+import platform
+import logging
+import random
+import json
+import sys
+import os
+import re
+
+import discord
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot, Context
+
+if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
+        config = json.load(file)
+
+intents = discord.Intents.all()
+
+bot = Bot(
+    command_prefix=commands.when_mentioned_or(config["prefix"]),
+    intents=intents,
+    help_command=None,
+)
+
+class LoggingFormatter(logging.Formatter):
+    # Colors
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    # Styles
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
+
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
+
+    def format(self, record):
+        log_color = self.COLORS[record.levelno]
+        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        format = format.replace("(black)", self.black + self.bold)
+        format = format.replace("(reset)", self.reset)
+        format = format.replace("(levelcolor)", log_color)
+        format = format.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
+        return formatter.format(record)
+
+
+logger = logging.getLogger("discord_bot")
+logerror = logging.getLogger("discord_bot_error")
+logger.setLevel(logging.INFO)
+logerror.setLevel(logging.ERROR)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LoggingFormatter())
+# File handler
+file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+file_handler_formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<4}] {name}: {message}", "%d-%m-%Y %H:%M:%S", style="{"
+)
+file_handler.setFormatter(file_handler_formatter)
+
+# Add the handlers
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+bot.logger = logger
+bot.logerror = logerror
+bot.config = config
+
+# Diretório do projeto Java
+diretorio_projeto = "C:/prodata/sig/"
+
+@bot.event
+async def on_ready():
+    bot.logger.info(f"\nConectado com {bot.user.name}!")
+    bot.logger.info(f"Versão API discord.py: {discord.__version__}")
+    bot.logger.info(f"Versão Python: {platform.python_version()}")
+    bot.logger.info(f"Rodando na plataforma: {platform.system()} {platform.release()} ({os.name})")
+    bot.logger.info("-------------------")
+    status_task.start()
+
+    if config["sync_commands_globally"]:
+        bot.logger.info("Sincronizado com comandos globais...\n")
+        await bot.tree.sync()
+
+@tasks.loop(minutes=1.0)
+async def status_task() -> None:
+    """
+    Definindo status do bot.
+    """
+    statuses = ["Os outros bots.", "Paciência!", "Com humanos!"]
+    await bot.change_presence(activity=discord.Game(random.choice(statuses)))
+
+@bot.command(name='branch')
+async def branch(contexto):
+
+    try:
+
+        await contexto.send('Consultando branch atual. \n')
+    
+        os.chdir(diretorio_projeto)
+        
+        diretorio_atual = os.getcwd()
+        arquivos = os.listdir(diretorio_projeto)
+        linhas = diretorio_atual.__str__()
+
+        await contexto.send(f"Diretorio Atual: {linhas}")
+
+        #for arquivo in arquivos:
+            #await contexto.send(arquivo)
+
+        branch = subprocess.run("git branch", capture_output=True, text=True)
+
+        resultado = branch.stdout.strip()
+        bot.logerror.error(f"TESTE")
+
+        await contexto.send(resultado)
+    
+    except Exception as exception:
+        bot.logerror.error(f"ERROR - {exception}")
+
+@bot.command(name='ajuda')
+async def ajuda(contexto):
+    await contexto.send('Menu de Ajuda, \n\n1 - Comandos')
+
+@bot.command(name='mudar-branch')
+async def mudar_branch(contexto):
+    print('')
+
+@bot.command(name='sobre')
+async def sobre(contexto):
+    await contexto.send('Bot feito com a intenção de automatizar o build da aplicação SIG,\nCriação: 02/06/2023, \nCriado e mantido por: Pablo Soares!')
+
+@bot.event
+async def on_message(message):
+    # Verifica se a mensagem foi enviada por um usuário (exclui mensagens do próprio bot)
+    if message.author != bot.user:
+
+        # Acessa o conteúdo da mensagem
+        conteudo = message.content
+
+        bot.logger.info(f"Conteudo: {conteudo}")
+
+        if conteudo.startswith(config["prefix"]):
+
+            partes = conteudo.split(' ') 
+            comando = partes[0][len(config["prefix"]):]
+
+            # Verificando comunicação com os canais
+            resposta = "iniciando processamento!\n"
+            await message.channel.send(resposta)
+
+            bot.logger.info(f"Comando recebido: {comando}")  # Adiciona um print para exibir o comando recebido
+            
+            try:
+            
+                if comando == 'comando1':
+                    await message.channel.send('Executando o comando1!')
+                elif comando == 'comando2':
+                    await message.channel.send('Executando o comando2!')
+                elif comando == 'mudar-branch':
+                    
+                    await message.channel.send('Processo de checkout iniciado:')
+                    
+                    branch = partes[1]
+                    processo = await checkout(branch)
+                    resultado = await regex(processo)
+
+                    bot.logger.info(f"Args: {resultado.args}")
+                    bot.logger.info(f"Returncode: {resultado.returncode}")
+                    bot.logger.info(f"Stderr: {resultado.stderr}")
+
+                    if resultado.returncode == 0:
+                        bot.logger.info("Sucesso na alteração da branch!")
+
+                        message = discord.Message(content='!branch')
+                        ctx = await bot.get_context(message)
+
+                        await bot.invoke("ctx")
+
+            except Exception as exception:
+
+                bot.logerror.error(f"ERROR - {exception}")
+
+    # Permite que o bot continue processando outros comandos
+    await bot.process_commands(message)
+    
+async def checkout(branch):
+    
+    try:
+
+        bot.logger.info(f'Mudando para a branch: {branch}')
+
+        os.chdir(diretorio_projeto)
+        
+        diretorio_atual = os.getcwd()
+        arquivos = os.listdir(diretorio_projeto)
+        #diretorio_atual = subprocess.run("dir", capture_output=True, universal_newlines=True, shell=True)
+        #linhas = diretorio_atual.__str__()
+
+        bot.logger.info(f"Diretorio Atual: {diretorio_atual}")
+
+        mudar_branch = subprocess.run(f"git checkout {branch}", capture_output=True)
+        mudar_branch = str(mudar_branch)
+
+        #jsonbranch = json.dumps(mudar_branch)
+
+        if mudar_branch.find("Your branch is up to date with"):
+            bot.logger.info("Branch Alterada com sucesso!")
+
+        #print(mudar_branch)
+        #print(jsonbranch)
+
+        #for arquivo in arquivos:
+            #await contexto.send(arquivo)
+
+        return(mudar_branch)
+    
+    except Exception as exeption:
+        bot.logerror.error(f"ERROR - {exeption}")
+        return("Erro ao alterar branch")
+
+class ResultadoRegex:
+    def __init__(self, args, returncode, stdout, stderr):
+        self.args = args
+        self.returncode = returncode
+        self.stdout = stdout
+        self.stderr = stderr
+    
+async def regex(texto):
+
+    try:
+        # Extrair os argumentos entre parênteses
+        args_match = re.search(r"args='(.*?)'", texto)
+        args = args_match.group(1) if args_match else ""
+
+        # Extrair o valor de returncode
+        returncode_match = re.search(r"returncode=(\d+)", texto)
+        returncode = int(returncode_match.group(1)) if returncode_match else 0
+
+        # Extrair o valor de stdout
+        stdout_match = re.search(r"stdout=b\"(.*?)\"", texto)
+        stdout = stdout_match.group(1) if stdout_match else ""
+
+        # Extrair o valor de stderr
+        stderr_match = re.search(r"stderr=b\"(.*?)\"", texto)
+        stderr = stderr_match.group(1) if stderr_match else ""
+
+        return ResultadoRegex(args, returncode, stdout, stderr)
+
+    except Exception as exception:
+        bot.logerror.error(f'ERROR - {exception}')
+        return None
+
+bot.run(config["token"])
