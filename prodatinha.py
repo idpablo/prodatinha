1e99bc743c2070b523df376201518d3fb46858cc
idpablo <pbs.97@outlook.com>
2023-06-10 18:05:11 -0300

    Separando a função regex

--- prodatinha.py
+++ prodatinha.py
@@ -4,11 +4,15 @@ import random
 import json
 import sys
 import os
-import re
 
 import discord
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot, Context
+
+from projeto import atualizar_projeto
+from traduzir import traduzir_texto
+from logger import setup_logger
+from regex import regex
 
 if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
@@ -24,8 +28,6 @@ bot = Bot(
     help_command=None,
 )
 
-from logger import setup_logger
-
 # Setup loggers
 logger = setup_logger("discord_bot", "discord.log")
 logerror = setup_logger("discord_bot_error", "discord.log")
@@ -34,14 +36,13 @@ bot.logger = logger
 bot.logerror = logerror
 bot.config = config
 
-bot.config = config
-
 # Diretório do projeto Java
-diretorio_projeto = "C:/prodata/sig/"
+diretorio_projeto = "C:/prodata/sig"
+diretorio_sig = "C:/prodata/sig/sig"
 
 @bot.event
 async def on_ready():
-    bot.logger.info(f"\nConectado com {bot.user.name}!")
+    bot.logger.info(f"Conectado com {bot.user.name}!")
     bot.logger.info(f"Versão API discord.py: {discord.__version__}")
     bot.logger.info(f"Versão Python: {platform.python_version()}")
     bot.logger.info(f"Rodando na plataforma: {platform.system()} {platform.release()} ({os.name})")
@@ -64,29 +65,28 @@ async def status_task() -> None:
 async def branch(contexto):
 
     try:
-
-        await contexto.send('Consultando branch atual. \n')
     
         os.chdir(diretorio_projeto)
         
         diretorio_atual = os.getcwd()
         arquivos = os.listdir(diretorio_projeto)
-        linhas = diretorio_atual.__str__()
 
-        await contexto.send(f"Diretorio Atual: {linhas}")
+        listabranch = (subprocess.run("git branch", capture_output=True, text=True)).stdout.strip()
 
-        #for arquivo in arquivos:
-            #await contexto.send(arquivo)
-
-        branch = subprocess.run("git branch", capture_output=True, text=True)
-
-        resultado = branch.stdout.strip()
-        bot.logerror.error(f"TESTE")
-
-        await contexto.send(resultado)
+        await contexto.send(f"Diretorio: {diretorio_atual}")
+        await contexto.send(f"Branch atual >>> \n{listabranch}")
+        bot.logger.info(f"Branch atual: {listabranch.split()}")
     
     except Exception as exception:
+
         bot.logerror.error(f"ERROR - {exception}")
+
+@bot.command(name='gerar-versao-sig')
+async def gerar_versao_sig(contexto):
+    bot.logger.info('Iniciando processo de build.')
+
+    # Chamar a função para atualizar o projeto
+    await atualizar_projeto(bot, diretorio_projeto, diretorio_sig)
 
 @bot.command(name='ajuda')
 async def ajuda(contexto):
@@ -115,42 +115,50 @@ async def on_message(message):
             partes = conteudo.split(' ') 
             comando = partes[0][len(config["prefix"]):]
 
-            # Verificando comunicação com os canais
-            resposta = "iniciando processamento!\n"
+            resposta = "iniciando sistemas...\n"
             await message.channel.send(resposta)
 
             bot.logger.info(f"Comando recebido: {comando}")  # Adiciona um print para exibir o comando recebido
             
             try:
             
-                if comando == 'comando1':
-                    await message.channel.send('Executando o comando1!')
-                elif comando == 'comando2':
-                    await message.channel.send('Executando o comando2!')
-                elif comando == 'mudar-branch':
+                if comando == 'mudar-branch':
                     
-                    await message.channel.send('Processo de checkout iniciado:')
+                    await message.channel.send('Checkout iniciado...')
                     
                     branch = partes[1]
                     processo = await checkout(branch)
-                    resultado = await regex(processo)
+                    resultado = await regex(bot, processo)
 
                     bot.logger.info(f"Args: {resultado.args}")
                     bot.logger.info(f"Returncode: {resultado.returncode}")
                     bot.logger.info(f"Stderr: {resultado.stderr}")
+                    
+                    await message.channel.send(f'Mudando para a branch --> {branch}')
 
                     if resultado.returncode == 0:
-                        bot.logger.info("Sucesso na alteração da branch!")
+            
+                        bot.logger.info(f"Branch alterada - {resultado.stderr}")
+            
+                        await message.channel.send(f"Processamento sendo executado --> {resultado.args}")
+                        await message.channel.send(f"Branch Alterada --> {resultado.stderr}\n\n")
+                        await message.channel.send("\nSucesso na alteração da branch!")
 
-                        message = discord.Message(content='!branch')
-                        ctx = await bot.get_context(message)
+                    elif resultado.returncode == 1:
+                    
+                        bot.logerror.error(f"{resultado.stderr}")
+                        await message.channel.send(f"Falha ao alterar a branch - {resultado.stderr}")
+                 
+                elif comando == 'gerar-versao-sig':
+                    
+                    if os.chdir == diretorio_projeto:
 
-                        await bot.invoke("ctx")
+                        atualizar_projeto = subprocess
 
             except Exception as exception:
 
                 bot.logerror.error(f"ERROR - {exception}")
-
+       
     # Permite que o bot continue processando outros comandos
     await bot.process_commands(message)
     
@@ -158,63 +166,22 @@ async def checkout(branch):
     
     try:
 
-        bot.logger.info(f'Mudando para a branch: {branch}')
-
         os.chdir(diretorio_projeto)
-        
         diretorio_atual = os.getcwd()
-        arquivos = os.listdir(diretorio_projeto)
-        #diretorio_atual = subprocess.run("dir", capture_output=True, universal_newlines=True, shell=True)
-        #linhas = diretorio_atual.__str__()
 
-        bot.logger.info(f"Diretorio Atual: {diretorio_atual}")
+        bot.logger.info(f'Mudando para a branch --> {branch}')
+        bot.logger.info(f"Diretorio: {diretorio_atual}")
 
         mudar_branch = subprocess.run(f"git checkout {branch}", capture_output=True)
         mudar_branch = str(mudar_branch)
 
-        #jsonbranch = json.dumps(mudar_branch)
-
         if mudar_branch.find("Your branch is up to date with"):
             bot.logger.info("Branch Alterada com sucesso!")
-
-        #print(mudar_branch)
-        #print(jsonbranch)
-
-        #for arquivo in arquivos:
-            #await contexto.send(arquivo)
 
         return(mudar_branch)
     
     except Exception as exeption:
         bot.logerror.error(f"ERROR - {exeption}")
         return("Erro ao alterar branch")
-
-class ResultadoRegex:
-    def __init__(self, args, returncode, stdout, stderr):
-        self.args = args
-        self.returncode = returncode
-        self.stdout = stdout
-        self.stderr = stderr
-    
-async def regex(texto):
-
-    try:
-        args_match = re.search(r"args='(.*?)'", texto)
-        args = args_match.group(1) if args_match else ""
-
-        returncode_match = re.search(r"returncode=(\d+)", texto)
-        returncode = int(returncode_match.group(1)) if returncode_match else 0
-
-        stdout_match = re.search(r"stdout=b\"(.*?)\"", texto)
-        stdout = stdout_match.group(1) if stdout_match else ""
-
-        stderr_match = re.search(r"stderr=b\"(.*?)\"", texto)
-        stderr = stderr_match.group(1) if stderr_match else ""
-
-        return ResultadoRegex(args, returncode, stdout, stderr)
-
-    except Exception as exception:
-        bot.logerror.error(f'ERROR - {exception}')
-        return None
 
 bot.run(config["token"])
