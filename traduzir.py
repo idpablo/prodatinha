944354fccf096503757e3a7ff59e6a407674047d
idpablo <pbs.97@outlook.com>
2023-06-10 18:05:34 -0300

    Criando a função traduzir para realizar a tradução do output do git

---
+++ traduzir.py
@@ -0,0 +1,6 @@
+from googletrans import Translator
+
+async def traduzir_texto(texto, destino='pt'):
+    translator = Translator()
+    traducao = translator.translate(texto, dest=destino)
+    return traducao.text
