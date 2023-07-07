8ec9879c1422d61c55c073922774a5edf05464db
Pablo <pablosoares@ip-10-30-1-70.ec2.internal>
2023-07-07 13:49:16 +0000

    Atualizando configuração de ambiente

--- permision.sh
+++ permision.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 chmod +x prodatinha.py
+chmod +x config/__init__.py
 chmod +x config/config.py
-chmod +x resource/apm.py
+chmod +x resource/__init__.py
 chmod +x resource/projeto.py
 chmod +x resource/status_war_gerado.py
+chmod +x resource/upload.py
+chmod +x util/__init__.py
 chmod +x util/logger.py
 chmod +x util/regex.py
 chmod +x util/traduzir.py
