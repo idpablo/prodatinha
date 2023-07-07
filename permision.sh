c1bdfd7eaa1ed10a6435251e882bb1568ec4bd91
Pablo <pablosoares@ip-10-30-1-70.ec2.internal>
2023-07-07 13:42:43 +0000

    adicionado arquivo de permisão

---
+++ permision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod +x prodatinha.py
+chmod +x config/config.py
+chmod +x resource/apm.py
+chmod +x resource/projeto.py
+chmod +x resource/status_war_gerado.py
+chmod +x util/logger.py
+chmod +x util/regex.py
+chmod +x util/traduzir.py
+
